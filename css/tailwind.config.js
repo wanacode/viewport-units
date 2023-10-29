709afe8741cd419a6953a171a9c9087d58914257
Fraser <frasernz@gmail.com>
2023-10-29 11:00:41 -0500

    commit

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",],
+  // content: ["./index.html",],
+  content: ["./flex.html",],
   theme: {
     extend: {},
   },
